a4966b7f4d5512699cd0c771da48914f29d1f656
EVANDRO MACHADO PEREIRA <evandromachadopereira@hotmail.com>
2024-11-05 20:32:38 -0300

    Live URL

--- src/Pages/Live.jsx
+++ src/Pages/Live.jsx
@@ -14,7 +14,7 @@ export default function Live(){
 
             <div className='bg-slate-600 p-4 pb-2 m-4 rounded-lg relative' style={{minWidth:"640px", minHeight:"480px"}}>
                 <CircularProgress color='white' size={80} className='absolute' style={{display: loading ? "block" : "none", transform: "translate(-50%, -50%)", top:"50%", left:"50%"}}/>
-                <img src='http://150.162.233.207:8081' onLoad={() => setLoading(false)} />
+                <img src='https://cam.rexlab.ufsc.br/motion' onLoad={() => setLoading(false)} />
                 
                 <Typography className='pt-2'>
                 Laboratório de Experimentação Remota - UFSC Araranguá
